f5e8b5058ade135975a2ed7c88f08465c520779a
akirtley <alexkirtley5@gmail.com>
2018-08-03 22:14:59 -0700

    Finished list comprehension lecture

---
+++ section_2_python_statements/list_comprehension.py
@@ -0,0 +1,31 @@
+def main():
+
+    # one method of splitting string
+    my_string = 'hello'
+    my_list = []
+    for letter in my_string:
+        my_list.append(letter)
+    print(my_list)
+
+    # same thing, one line
+    my_list = [letter for letter in my_string]
+    print(my_list)
+
+    new_list = [num for num in range(0,11)]
+    print(new_list)
+    new_list = [num**2 for num in range(0,11)]
+    print(new_list)
+
+    # can have conditionals inside
+    new_list = [num for num in range(0,11) if num%2==0]
+    print(new_list)
+
+    # can perform calculations inside
+    degrees_C = [0,10,20,100]
+    degrees_F = [((9/5)*temp+32) for temp in degrees_C]
+    print(degrees_C)
+    print(degrees_F)
+
+
+    pass
+main()
